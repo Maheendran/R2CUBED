68cb06528e7ff7c9917a2043f984e797bbc534b8
maheendran kp <maheendrankp100@gmail.com>
2024-03-25 09:11:06 +0530

    head updated

--- src/app/pages/banner/page.jsx
+++ src/app/pages/banner/page.jsx
@@ -276,11 +276,11 @@ gsap.timeline({
   
 
   <div className='leftbox absolute  top-0 bottom-0 w-[45%] h-fit my-auto '>
-    <h1 className='text-[3rem] text-center mt-5 text-neutral-200'>Heading</h1>
+    <h1 className='text-[3rem] text-center mt-5 text-neutral-200'>Subheading-1</h1>
     <p className='text-[1rem] text-justify text-neutral-400 '>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam, excepturi sed, itaque placeat id natus soluta veniam obcaecati qui a laborum laboriosam dolorem illum labore sit, voluptates commodi neque dolores tempore temporibus deleniti? Nobis, ratione hic error quis cum neque nulla laudantium nostrum sit nihil dolorum quisquam enim quaerat, eaque ex sequi, harum totam quia non! Labore, neque! Amet voluptatem illo similique recusandae! Ex quaerat quibusdam asperiores, ducimus tempore magni labore. Tenetur voluptas, quos ex repellendus provident mollitia laboriosam adipisci alias a impedit, cum accusamus rerum delectus eaque facilis veniam quia laborum incidunt ea assumenda! Eos magni aspernatur quod distinctio.</p>
   </div>
   <div className='rightbox absolute    right-[1rem] bottom-0 top-0  w-[45%] h-fit my-auto '>
-    <h1 className='text-[3rem] text-center mt-5 text-neutral-200'>Heading</h1>
+    <h1 className='text-[3rem] text-center mt-5 text-neutral-200'>Subheading-2</h1>
     <p className='text-[1rem] text-justify text-neutral-400'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam, excepturi sed, itaque placeat id natus soluta veniam obcaecati qui a laborum laboriosam dolorem illum labore sit, voluptates commodi neque dolores tempore temporibus deleniti? Nobis, ratione hic error quis cum neque nulla laudantium nostrum sit nihil dolorum quisquam enim quaerat, eaque ex sequi, harum totam quia non! Labore, neque! Amet voluptatem illo similique recusandae! Ex quaerat quibusdam asperiores, ducimus tempore magni labore. Tenetur voluptas, quos ex repellendus provident mollitia laboriosam adipisci alias a impedit, cum accusamus rerum delectus eaque facilis veniam quia laborum incidunt ea assumenda! Eos magni aspernatur quod distinctio.</p>
   </div>
 
