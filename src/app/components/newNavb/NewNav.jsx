a0a0b7b074d9b48805ce4de66c40d4f4e9191754
maheendran kp <maheendrankp100@gmail.com>
2024-03-30 12:36:57 +0530

    btn updated

--- src/app/components/newNavb/NewNav.jsx
+++ src/app/components/newNavb/NewNav.jsx
@@ -64,7 +64,7 @@ const NewNav = () => {
             </svg>
           </div>
 
-           <div id="" className=" menuOne flex gap-3 absolute    top-0 bottom-0 my-auto w-[25vw] z-[-100]">
+           <div id="" className=" menuOne flex gap-3 absolute    top-0 bottom-0 my-auto w-[30vw] z-[-100]">
           <p className="my-auto menuNav text-[0.9rem]">Service</p>
           <p className="my-auto menuNav text-[0.9rem]">About</p>
           <p className="my-auto menuNav text-[0.9rem]">projects</p>
