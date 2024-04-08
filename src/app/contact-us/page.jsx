83ee803c3b0ff1ff09f43a995039b061420a4f4b
maheendran kp <maheendrankp100@gmail.com>
2024-04-08 12:56:42 +0530

    project addede

--- src/app/contact-us/page.jsx
+++ src/app/contact-us/page.jsx
@@ -2,10 +2,54 @@ import React from 'react'
 import NewNav from '../components/newNavb/NewNav'
 
 const Page = () => {
-  return (
-    <div className='w-full h-screen '>
+  return ( 
+    <div className='w-full h-screen  '>
         <NewNav/>
-      contact us</div>
+{/* 
+        <div className='w-full h-[91.5vh] '>
+          <img className='h-full mx-auto' src="/contact.png" alt="" />
+          <div className='absolute p-4 top-0 bottom-0 left-0 right-0 m-auto w-[30%] h-[80%] rounded-3xl contactForm
+          flex flex-col justify-between'>
+          <input
+            type="text"
+            placeholder="Name"
+            className="contactInput p-2 mb-1 bg-red-300 w-[100%] text-[2rem] bg-transparent text-white border-0 border-b-2"
+          />
+   <input
+            type="text"
+            placeholder="Email"
+            className="contactInput p-2 mb-1 bg-red-300 w-[100%] text-[2rem] bg-transparent text-white border-0 border-b-2"
+          />
+
+<input
+            type="text"
+            placeholder="Phone"
+            className="contactInput p-2 mb-1 bg-red-300 w-[100%] text-[2rem] bg-transparent text-white border-0 border-b-2"
+          />
+<textarea
+            type="text"
+            placeholder="message"
+            className="contactInput p-2 mb-1 bg-red-300 w-[100%] h-[30vh] text-[2rem] bg-transparent text-white border-0 border-b-2"
+          />
+<div className='w-full flex'>
+<button class="relative  hover:border-sky-600 duration-500 group cursor-pointer text-sky-50  overflow-hidden h-14 w-56 rounded-md bg-sky-800 p-2 flex justify-center items-center font-extrabold
+mx-auto">
+  <div class="absolute z-10 w-48 h-48 rounded-full group-hover:scale-150 transition-all  duration-500 ease-in-out bg-sky-900 delay-150 group-hover:delay-75"></div>
+  <div class="absolute z-10 w-40 h-40 rounded-full group-hover:scale-150 transition-all  duration-500 ease-in-out bg-sky-800 delay-150 group-hover:delay-100"></div>
+  <div class="absolute z-10 w-32 h-32 rounded-full group-hover:scale-150 transition-all  duration-500 ease-in-out bg-sky-700 delay-150 group-hover:delay-150"></div>
+  <div class="absolute z-10 w-24 h-24 rounded-full group-hover:scale-150 transition-all  duration-500 ease-in-out bg-sky-600 delay-150 group-hover:delay-200"></div>
+  <div class="absolute z-10 w-16 h-16 rounded-full group-hover:scale-150 transition-all  duration-500 ease-in-out bg-sky-500 delay-150 group-hover:delay-300"></div>
+  <p class="z-10">Send</p>
+</button>
+
+
+
+</div>
+
+
+          </div>
+        </div> */}
+   </div>
   )
 }
 
