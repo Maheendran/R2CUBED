9dd9eb0dfc1773ea3e80985bf7807025ae6a89a9
maheendran kp <maheendrankp100@gmail.com>
2024-04-08 19:30:19 +0530

    update new

--- src/app/contact-us/page.jsx
+++ src/app/contact-us/page.jsx
@@ -1,12 +1,72 @@
-import React from 'react'
-import NewNav from '../components/newNavb/NewNav'
+import React from "react";
+import NewNav from "../components/newNavb/NewNav";
 
 const Page = () => {
-  return ( 
-    <div className='w-full h-screen  '>
-        <NewNav/>
-{/* 
-        <div className='w-full h-[91.5vh] '>
+  return (
+    <div className="w-full h-screen  ">
+      <NewNav />
+      {/* 
+     
+        */}
+      {/* ============================ */}
+
+      <section className="text-gray-600 body-font  h-[91.5vh] w-full ">
+        <div
+          className="container px-5  h-fit absolute p-4 top-0 bottom-0 left-0 right-0 m-auto 
+     flex sm:flex-nowrap flex-wrap"
+        >
+          <div className="lg:w-2/3 md:w-1/2 bg-black rounded-lg overflow-hidden sm:mr-10  flex items-end justify-start relative">
+            <img src="/contact.png" alt="" />
+          </div>
+          <div className="lg:w-1/3 md:w-1/2 text-white p-4 rounded-xl bg-black flex flex-col md:ml-auto w-full  mt-8 md:mt-0">
+            <h2 className="text-lg mb-1 font-medium title-font">Contact-Us</h2>
+            <p className="leading-relaxed mb-5 ">
+              R2CUBED smms csccdcdcsdcds dfdsv
+            </p>
+            <div className="relative mb-4">
+              <label for="name" className="leading-7 text-sm ">
+                Name
+              </label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                className="w-full bg-white rounded border border-gray-300 focus:border-red-500 focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              />
+            </div>
+            <div className="relative mb-4">
+              <label for="email" className="leading-7 text-sm ">
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                className="w-full bg-white rounded border border-gray-300 focus:border-red-500 focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              />
+            </div>
+            <div className="relative mb-4">
+              <label for="message" className="leading-7 text-sm ">
+                Message
+              </label>
+              <textarea
+                id="message"
+                name="message"
+                className="w-full bg-white rounded border border-gray-300 focus:border-red-500 focus:ring-2 focus:ring-red-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+              ></textarea>
+            </div>
+            <button className="text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded text-lg">
+              Send
+            </button>
+            <p className="text-xs  mt-3">
+              aaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaa
+            </p>
+          </div>
+        </div>
+      </section>
+      {/* /================== */}
+
+      <div className='w-full h-[91.5vh] relative'>
           <img className='h-full mx-auto' src="/contact.png" alt="" />
           <div className='absolute p-4 top-0 bottom-0 left-0 right-0 m-auto w-[30%] h-[80%] rounded-3xl contactForm
           flex flex-col justify-between'>
@@ -48,9 +108,9 @@ mx-auto">
 
 
           </div>
-        </div> */}
-   </div>
-  )
-}
+          </div>
+    </div>
+  );
+};
 
-export default Page
+export default Page;
