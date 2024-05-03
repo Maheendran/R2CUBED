bde68149e678ac1dd44a89f0ddf7140ae3027d28
maheendran kp <maheendrankp100@gmail.com>
2024-05-03 13:41:42 +0530

    about page responsive

--- src/app/components/circle/Circle.jsx
+++ src/app/components/circle/Circle.jsx
@@ -1,43 +1,75 @@
 "use client";
-import React from 'react'
-
+import React, { useEffect } from "react";
 
 import FancyCarousel from "react-fancy-circular-carousel";
-import 'react-fancy-circular-carousel/FancyCarousel.css';
+import "react-fancy-circular-carousel/FancyCarousel.css";
 // import Info from './Info';
 
-import { useState } from 'react';
+import { useState } from "react";
 
 const Circle = () => {
-    const [focusElement, setFocusElement] = useState(0);
-    const images = ['/Image/1.png', '/Image/7.png','/Image/5.png','/Image/1.png','/Image/3.png','/Image/7.png'];
-    const info = [ 'Boosted Productivity',
-                 'Reduced Downtime',
-                  'Optimized Processes',
-                  'Improved Quality',
-                  ' Easier Upskilling ',
-                  'Simplified Compliance'];
-  return (
-    <div className="carousel  text-white relative w-[65%] mx-auto my-[4rem]">
-    <div className='maincircle'>
-   <FancyCarousel 
-   images={images} 
-   setFocusElement={setFocusElement}
-   carouselRadius={150}
-      peripheralImageRadius={40}
-      centralImageRadius={50}
-      focusElementStyling={{border: '2px solid #ba4949'}}
-      autoRotateTime={1}
-      borderWidth={4}
-      borderHexColor={'1c364f'}
-   />
-      <div className=" h-fit absolute text-white
-       top-0 right-0 bottom-0 my-auto">
-        <p > {info[focusElement]} </p>
-      </div>
-      </div>
-  </div>
-  )
-}
+  const screenSize = window.screen.width;
+  const [childImg, setChildImg] = useState(40);
+  const [carouselRadius, setCarouselRadius] = useState(150);
+  const [centralImageRadius, setCentralImageRadius] = useState(50);
+  useEffect(() => {
+    console.log(screenSize, "screenSize");
 
-export default Circle
+    if (screenSize < 480) {
+      setChildImg(25);
+      setCarouselRadius(120);
+      setCentralImageRadius(50);
+      return
+    }
+    if (screenSize < 766) {
+    
+      setChildImg(30);
+      setCarouselRadius(140);
+      setCentralImageRadius(55);
+      return
+    }
+  }, [screenSize]);
+  const [focusElement, setFocusElement] = useState(0);
+  const images = [
+    "/Image/1.png",
+    "/Image/7.png",
+    "/Image/5.png",
+    "/Image/1.png",
+    "/Image/3.png",
+    "/Image/7.png",
+  ];
+  const info = [
+    "Boosted Productivity",
+    "Reduced Downtime",
+    "Optimized Processes",
+    "Improved Quality",
+    " Easier Upskilling ",
+    "Simplified Compliance",
+  ];
+  return (
+    <div className="carousel relative  w-full md:w-[45%] lg:w-[65%] h-fit md:h-[50vh]    text-white   mx-auto my-[4rem]">
+      <div className="maincircle ml-[1.3rem]  absolute left-0 right-0 bottom-0 top-0  mx-auto">
+        <FancyCarousel
+          images={images}
+          setFocusElement={setFocusElement}
+          carouselRadius={carouselRadius}
+          peripheralImageRadius={childImg}
+          centralImageRadius={centralImageRadius}
+          focusElementStyling={{ border: "2px solid #ba4949" }}
+          autoRotateTime={1}
+          borderWidth={4}
+          borderHexColor={"1c364f"}
+        />
+      </div>
+
+      <div
+        className=" h-fit  mt-[48vh] md:mt-[52vh] lg:absolute text-white
+       lg:top-0  lg:right-0 bottom-0  text-center lg:my-auto"
+      >
+        <p> {info[focusElement]} </p>
+      </div>
+    </div>
+  );
+};
+
+export default Circle;
--- src/app/manufacturing/page.jsx
+++ src/app/manufacturing/page.jsx
@@ -8,12 +8,10 @@ const Page = () => {
     <>
       <NewNav />
       {/* bg-red-200 sm:bg-green-300 md:bg-blue-300 */}
-      <div className="
-       w-full h-[36vh] md:h-[91.5vh]  relative  ">
-        
+      <div  className="w-full h-[36vh] md:h-[91.5vh]  relative  "
+      >
         <div className="hidden md:black w-[60%] md:w-[55%] h-full md:flex p-[3rem]">
           <div className="w-full text-justify z-10 text-[1.5rem]  leading-[2.3rem]  my-auto">
-        
             <p>
               R2Cube's
               <span className="bg-black text-white p-2 ml-[0.5rem]">
@@ -26,57 +24,59 @@ const Page = () => {
           </div>
         </div>
 
-<div className="absolute w-[70%] right-0 top-0  h-full">
- <img
-          src="/6.png"
-          className=" absolute w-full right-0 top-0  h-full"
-          alt=""
-        />
-        <img
-          src="/6.png"
-          className=" absolute w-full right-0 top-0  h-full"
-          alt=""
-        />
-</div>
-       
+        <div className="absolute w-[70%] right-0 top-0  h-full">
+          <img
+            src="/6.png"
+            className=" absolute w-full right-0 top-0  h-full"
+            alt=""
+          />
+          <img
+            src="/6.png"
+            className=" absolute w-full right-0 top-0  h-full"
+            alt=""
+          />
+        </div>
 
         <Link href="/enquiry">
-
-          <button class="button2 
+          <button
+            class="button2 
           absolute md:left-[25%]  md:bottom-[6rem] w-fit h-fit 
           left-[10%] bottom-[43%] my-auto
-          ">
+          "
+          >
             Book A Demo
           </button>
         </Link>
-  
-      </div> 
-           <div className="black w-[full] md:hidden 
-            h-fit flex p-[1.5rem]">
-          <div className="w-full text-justify z-10 text-[1.2rem]  leading-[2.3rem]  my-auto">
-        
-            <p>
-              R2Cube's
-              <span className="bg-black text-white p-2 ml-[0.5rem]">
-                'SENSOL'
-              </span>{" "}
-              is an end-to-end solution designed to significantly enhance uptime
-              and productivity in any factory or manufacturing operation, be it
-              discrete or continuous.
-            </p>
-          </div>
+      </div>
+      <div
+        className="black w-[full] md:hidden 
+            h-fit flex p-[1.5rem]"
+      >
+        <div className="w-full text-justify z-10 text-[1.2rem]  leading-[2.3rem]  my-auto">
+          <p>
+            R2Cube's
+            <span className="bg-black text-white p-2 ml-[0.5rem]">
+              'SENSOL'
+            </span>{" "}
+            is an end-to-end solution designed to significantly enhance uptime
+            and productivity in any factory or manufacturing operation, be it
+            discrete or continuous.
+          </p>
         </div>
+      </div>
       {/* =========== */}
 
-      <div className="w-full h-[50vh] text-white  bg-black flex justify-around">
-        <div className="w-[20%] h-[50%] my-auto  flex flex-col">
-          <div className="h-[30%]  mb-[1.5rem]">
+      <div className="w-full h-fit md:h-[50vh] text-white  bg-black
+       flex flex-col gap-[3rem] md:gap-[0rem] md:flex-row justify-around relative p-[2rem] md:p-0">
+    
+        <div className="w-[80%]  text-center md:text-start mx-auto md:w-[20%]  h-fit  md:h-[50%] my-auto  flex flex-col">
+          <div className="h-[30%]   mb-[1.5rem]">
             <p className="text-[2rem] font-semibold">15%</p>
           </div>
           <p className="text-[1.5rem]">Increase in OEE</p>
         </div>
 
-        <div className="w-[20%] h-[50%] my-auto  flex flex-col">
+        <div className="w-[80%]  text-center md:text-start  mx-auto md:w-[20%]     h-fit my-auto  flex flex-col">
           <div className="h-[30%]  mb-[1.5rem]">
             <p className="text-[2rem] font-semibold">12%</p>
           </div>
@@ -85,17 +85,20 @@ const Page = () => {
           </p>
         </div>
 
-        <div className="w-[20%] h-[50%] my-auto  flex flex-col">
+        <div className="w-[80%]  text-center md:text-start mx-auto md:w-[20%]    h-fit  my-auto  flex flex-col">
           <div className="h-[30%]  mb-[1.5rem]">
             <p className="text-[2rem] font-semibold">54%</p>
           </div>
           <p className="text-[1.5rem]">Increase in Equipment Uptime</p>
         </div>
+
       </div>
       {/* =========== */}
-      <div className="w-full h-[100vh]  relative  ">
-        <div className="w-[70%] relative  h-full flex">
-          <div className="w-[60%] h-fit absolute top-[2rem] right-0 bottom-0 left-0   m-auto text-justify z-10 text-[1.5rem]  leading-[2.3rem]  my-auto">
+
+      <div className="w-full   md:h-[100vh]  relative flex flex-col-reverse gap-[2rem] md:gap-0  ">
+        
+        <div className=" md:w-[70%] relative  h-full flex">
+          <div className="w-[90%] md:w-[60%] h-fit md:absolute top-[2rem] right-0 bottom-0 left-0   m-auto text-justify z-10 text-[1.5rem]  leading-[2.3rem]  my-auto">
             <p>
               Whether a facility is at Industry 1.0 or Industry 4.0 level,
               R2Cube's Industrial Metaverse offerings will
@@ -111,25 +114,49 @@ const Page = () => {
             </p>
           </div>
         </div>
+      
+      <div className=" relative md:absolute
+       w-full h-[50vh] md:w-[42%] md:h-[70%] z-50 right-0 bottom-0  my-auto">
         <img
           src="/8.png"
-          className="absolute w-[42%] h-[70%] z-50 right-0 bottom-0  my-auto"
+          className="absolute w-full h-full z-50 right-0 bottom-0  my-auto"
           alt=""
         />
         <img
           src="/8.png"
-          className="absolute w-[42%] h-[70%] z-50 right-0 bottom-0 my-auto"
+          className="absolute w-full h-full z-50 right-0 bottom-0 my-auto"
           alt=""
         />
       </div>
+        
+      </div>
+
       {/* =========== */}
-      <div className="w-full bg-black text-white h-fit flex flex-col  relative  text-[1.5rem] p-[3rem]">
-        <p>
+      <div className="w-full bg-black text-white h-fit flex flex-col  relative text-[1.2rem] md:text-[1.5rem] p-[1.5rem] md:p-[3rem]">
+        <p className="text-justify">
           The end result is early detection of potential failures, allowing for
           planned maintenance windows ahead of unplanned downtime. This leads
           to:
         </p>
-        {/* <div className="mx-auto   my-[2rem]">
+       <div className=" flex justify-center">
+           <Circle />
+       </div>
+     
+        <p>
+          This comprehensive solution includes IIOT solutions, Energy Management
+          solutions, Machine Vision Solutions, and utilizes ML, AI, and various
+          levels of Manufacturing Metaverse features.
+        </p>
+
+      </div>
+
+      <Footer />
+    </>
+  );
+};
+
+export default Page;
+ {/* <div className="mx-auto   my-[2rem]">
     <ul className="flex flex-col gap-[1.8rem] manufact_ul  font-semibold">
     <li>Boosted Productivity
 </li>
@@ -141,19 +168,3 @@ const Page = () => {
 Simplified Compliance</li>
 </ul>
 </div> */}
-        <Circle />
-        <p>
-          This comprehensive solution includes IIOT solutions, Energy Management
-          solutions, Machine Vision Solutions, and utilizes ML, AI, and various
-          levels of Manufacturing Metaverse features.
-        </p>
-
-        {/* <img className="absolute right-0 top-0 bottom-0 my-auto w-[40%]  h-[50vh]" src="/10.png" alt="" /> */}
-      </div>
-
-      <Footer />
-    </>
-  );
-};
-
-export default Page;
