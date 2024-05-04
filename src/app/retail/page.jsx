871a7dfac854c137d4205c25aadeda8b7377e016
maheendran kp <maheendrankp100@gmail.com>
2024-05-04 09:43:27 +0530

    retail page responsive

--- src/app/retail/page.jsx
+++ src/app/retail/page.jsx
@@ -5,18 +5,26 @@ import Footer from "../components/footer/Footer";
 const page = () => {
   return (
     <>
-  <NewNav/>
-    <div className="w-full h-[93vh] mx-auto relative bg-[#f6f6f7]">
-    <img src="/retail.png" className="w-full absolute top-0 right-0 bottom-0 left-0 m-auto  h-full" alt="" />
+      <NewNav />
+      <div className="w-full h-[40vh] md:h-[93vh] mx-auto relative bg-[#f6f6f7]">
+        <img
+          src="/retail.png"
+          className="w-full absolute top-0 right-0 bottom-0 left-0 m-auto  h-full"
+          alt=""
+        />
 
-      <img src="/retail.png" className="w-full absolute top-0 right-0 bottom-0 left-0 m-auto  h-full" alt="" />
+        <img
+          src="/retail.png"
+          className="w-full absolute top-0 right-0 bottom-0 left-0 m-auto  h-full"
+          alt=""
+        />
       </div>
-      <p className="text-[2rem] text-center font-semibold">
+      <p className=" w-[80%] mx-auto text-[1.3rem] md:text-[2rem] text-center font-semibold">
         R2Cube’s Data Science Offering in the Retail Sector
       </p>
 
-      <div className="flex justify-between  w-[90%] mx-auto my-[2rem] h-fit gap-[2rem]">
-        <div className="w-[28%] h-full  text-center text-[0.9rem]">
+      <div className="flex flex-col md:flex-row justify-between  w-[90%] mx-auto my-[2rem] h-fit gap-[2rem]">
+        <div className="md:w-[28%] h-full  text-center text-[0.9rem]">
           <img className="h-[40vh] w-[80%] mx-auto " src="/14.png" alt="" />
           <div className=" my-2  w-full py-3 text-[1rem]  bg-black text-center text-white">
             <p>Marketing & Customer Analytics</p>
@@ -40,7 +48,7 @@ const page = () => {
             in-stores)
           </p>
         </div>
-        <div className="w-[28%] h-full  text-center text-[0.9rem]">
+        <div className="md:w-[28%] h-full  text-center text-[0.9rem]">
           <img className="h-[60vh] w-full " src="/13.png" alt="" />
           <div className=" my-2  w-full py-3 text-[1rem] bg-black text-center text-white">
             <p>Brand Analytics</p>
@@ -51,29 +59,34 @@ const page = () => {
             Competition analysis based on reviews and ratings
           </p>
         </div>
-        <div className="w-[28%] h-full  text-center text-[0.9rem]">
+        <div className="md:w-[28%] h-full  text-center text-[0.9rem]">
           <img className="h-[40vh] w-[80%] mx-auto" src="/15.png" alt="" />
           <div className=" my-2  w-full py-3 text-[1rem]  bg-black text-center text-white">
             <p>Sales / revenue Analytics</p>
           </div>
 
-          <p>Trends, Seasonality, Forecasts and Revenue <br /> Optimization</p>
-          <p>Deep drive into product categories, customer
-segments, regions and channels <br />
-Pricing Models & Elasticity</p>
+          <p>
+            Trends, Seasonality, Forecasts and Revenue <br /> Optimization
+          </p>
+          <p>
+            Deep drive into product categories, customer segments, regions and
+            channels <br />
+            Pricing Models & Elasticity
+          </p>
         </div>
       </div>
-  
 
-    <div className="w-full h-screen mt-[2.8rem] relative">
-        <p className="text-[1.7rem] w-[80%] mt-[2rem] mx-auto text-center tracking-[.15em]">
-            "Leveraging   <span className="bg-black text-white p-2 ml-[0.2rem]">
-            Technology Partners 
-                </span>{" "}for Tracking KPIs and
-Regular Dashboard Updates"</p>
-        <img src="/17.png" className=" mx-auto w-[70%]  h-[80vh] " alt="" />
-    </div>
-    <Footer routeData={"contact-us"}/>
+      <div className="w-full h-fit md:h-screen mt-[2.8rem] relative">
+        <p className="text-[1.3rem] w-[95%] md:text-[1.7rem] md:w-[80%] mt-[2rem] mx-auto text-center tracking-[.15em]">
+          "Leveraging{" "}
+          <span className="bg-black text-white p-2 ml-[0.2rem]">
+            Technology Partners
+          </span>{" "}
+          for Tracking KPIs and Regular Dashboard Updates"
+        </p>
+        <img src="/17.png" className=" mx-auto w-[100%] md:w-[70%]  h-[35vh] md:h-[80vh] " alt="" />
+      </div>
+      <Footer routeData={"contact-us"} />
     </>
   );
 };
