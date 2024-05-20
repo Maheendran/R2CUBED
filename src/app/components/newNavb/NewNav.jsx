7e302c43cf6a2e5f56f28b88b74a8d1438212fff
Maheendran <maheendrankp007@gmail.com>
2024-05-20 19:05:57 +0530

    updated logo

--- src/app/components/newNavb/NewNav.jsx
+++ src/app/components/newNavb/NewNav.jsx
@@ -20,7 +20,7 @@ const NewNav = () => {
       overflow-hidden
        `}
         >
-          <div className="   w-[10.5rem] h-[7.8vh]  my-auto   px-2">
+          <div className="   w-[10.5rem] h-[7vh]  my-auto   px-2">
             <Link href="/">
               <img
                 id="logo"
